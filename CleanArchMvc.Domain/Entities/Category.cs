806e06886ee09b4e8a868f9f0d9b3cba071c8e4d
Wendell Santos <wendellsantooos@gmail.com>
2022-04-07 16:45:47 -0300

    Validações

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid ID");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required");
+            DomainExceptionValidation.When(name.Length < 3, 
+                "Invalid name, too short, minimum 3 caracters");
+
+            Name = name;
+        }
+    }
+}
