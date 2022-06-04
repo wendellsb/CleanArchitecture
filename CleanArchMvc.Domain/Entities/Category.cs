8ade1c1ab2929dc8747f78ea844be4645bbb7425
Wendell Santos <88899145+wendellsb@users.noreply.github.com>
2022-06-04 09:58:51 -0300

    04/06

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; } // criando uma coleção de produtos
+    }
+}
