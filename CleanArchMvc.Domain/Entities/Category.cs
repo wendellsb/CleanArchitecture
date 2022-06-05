0962548631242d154e081413235b86950854d79e
Wendell Santos <88899145+wendellsb@users.noreply.github.com>
2022-06-05 09:23:43 -0300

    05/06

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,39 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category // garantindo que a classe Category nao seja herdada
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        // construtores parametrizados
+        public Category(string name)
+        {
+            ValidadeDomain(name); //  testa condições de possiveis erros
+        }
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value"); // testa se o Id é negativo 
+            Id = id;
+            ValidadeDomain(name);
+        }
+
+        public void Update(string name) // metodo para permitir atualização de nome
+        {
+            ValidadeDomain(name);
+        }
 
         public ICollection<Product> Products { get; set; } // criando uma coleção de produtos
+
+        private void ValidadeDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required!");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters!");
+
+            Name = name;
+        }
     }
 }
--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,26 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Product
+    public sealed class Product // garantindo que a classe Product nao seja herdada
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get ; set; }
-        public string Image { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), // verifica se a name é nulo ou vazio
+                "Invalid name. Name is required!");
+
+            DomainExceptionValidation.When(name.Length < 3, // verifica se nome é menos que 3 caracters
+                "Invalid name, too short, minimum 3 characters!");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), // verifica se a description é nulo ou vazio
+                "Invalid name. Name is required!");
+        }
 
         // relacionando o produto com uma categoria
         public int CategoryId { get; set; }
